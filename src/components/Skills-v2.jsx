7a3fcdbea67dad7ddfa64f13dde5e0ef6feb598e
r-kimura <ryuumockingbird@gmail.com>
2023-07-17 18:15:21 +0900

    🎨

--- src/components/Skills-v2.jsx
+++ src/components/Skills-v2.jsx
@@ -8,19 +8,85 @@ const ITEMS = [
     skillName: "HTML",
     wrapperClassName: "skill-card html",
     iconClassName: "fa-brands fa-html5 html-icon",
-    proficiency: "100",
+    proficiency: "5",
+    taste: "Like!",
   },
   {
     skillName: "CSS",
     wrapperClassName: "skill-card css",
     iconClassName: "fa-brands fa-css3-alt css-icon",
-    proficiency: "100",
+    proficiency: "5",
+    taste: "Like!",
+  },
+  {
+    skillName: "Sass",
+    wrapperClassName: "skill-card css",
+    iconClassName: "fa-brands fa-sass css-icon",
+    proficiency: "5",
+    taste: "Like!",
+  },
+  {
+    skillName: "Bootstrap",
+    wrapperClassName: "skill-card css",
+    iconClassName: "fa-brands fa-bootstrap css-icon",
+    proficiency: "4",
+    taste: "Like!",
   },
   {
     skillName: "JavaScript",
     wrapperClassName: "skill-card js",
     iconClassName: "fa-brands fa-js-square js-icon",
-    proficiency: "80",
+    proficiency: "4",
+    taste: "",
+  },
+  {
+    skillName: "jQuery",
+    wrapperClassName: "skill-card js",
+    iconClassName: "js-icon",
+    proficiency: "4",
+    taste: "",
+  },
+  {
+    skillName: "React",
+    wrapperClassName: "skill-card js",
+    iconClassName: "fa-brands fa-react",
+    proficiency: "3( Learning Now! )",
+    taste: "",
+  },
+  {
+    skillName: "Next",
+    wrapperClassName: "skill-card js",
+    iconClassName: "fa-brands",
+    proficiency: "3 ( Learning Now! )",
+    taste: "",
+  },
+  {
+    skillName: "Vue",
+    wrapperClassName: "skill-card js",
+    iconClassName: "fa-brands fa-vuejs",
+    proficiency: "3",
+    taste: "",
+  },
+  {
+    skillName: "php",
+    wrapperClassName: "skill-card js",
+    iconClassName: "fa-brands fa-php",
+    proficiency: "3",
+    taste: "",
+  },
+  {
+    skillName: "Smarty",
+    wrapperClassName: "skill-card js",
+    iconClassName: "fa-brands",
+    proficiency: "5",
+    taste: "Like!",
+  },
+  {
+    skillName: "Git",
+    wrapperClassName: "skill-card js",
+    iconClassName: "fa-brands fa-square-git",
+    proficiency: "5",
+    taste: "Like!",
   },
 ];
 
@@ -30,11 +96,23 @@ const Skills = () => {
     <div className={`skills-container`}>
       <h2>Skills</h2>
 
+      <div className={styles.headlineBox}>
+        <h3 className={styles.headline}></h3>
+        <h3 className={styles.headline}>
+          Proficiency( 習熟度 ) <br />
+          <span className={styles.note}>
+            ※ 1 ~ 5<br />
+            1:軽く触った程度<br />
+            5:実務レベルで使用できる(または使用している)
+          </span>
+        </h3>
+        <h3 className={styles.headline}>Like or Not</h3>
+      </div>
+
       <div className={`${styles["grid-skills-v2"]} `}>
         {ITEMS.map((item) => {
           return (
             // <div className="skill-card">
-
             <>
               {/* <div className={item.wrapperClassName}> */}
               <div className={styles["f-container"]}>
@@ -42,8 +120,8 @@ const Skills = () => {
                   <i className={item.iconClassName}></i>
                   <p>{item.skillName}</p>
                 </div>
-                <div className={styles["f-item"]}>b</div>
-                <div className={styles["f-item"]}>c</div>
+                <div className={styles["f-item"]}>{item.proficiency}</div>
+                <div className={styles["f-item"]}>{item.taste}</div>
               </div>
               <br></br>
             </>
