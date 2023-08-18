f94b27635deecdc5f0ca1538425661ba3753118e
r-kimura <ryuumockingbird@gmail.com>
2023-08-18 10:58:59 +0900

    add shikaku

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,10 +12,19 @@ const About = () => {
           <p>
             千葉県松戸市出身、慶應義塾大学経済学部卒業。<br></br>
             南柏のWEBデザイナー養成学校でデザイン&コーディングを学ぶ。<br></br>
-            その後WEB制作業務を半年ほど行い、楽天グループ株式会社にWEBコーダーとして入社。
-            同社動画配信サービス『Rakuten TV』の各種キャンペーンページの製作や更新業務を担当中。
+            卒業後、制作会社にてコーディング業務を半年ほど行い、楽天グループ株式会社にWEBコーダーとして入社。
+            同社動画配信サービス『Rakuten
+            TV』の各種キャンペーンページの製作や更新業務を担当中。
           </p>
           <p>趣味は格闘技観戦、FPSゲーム、料理、など。 </p>
+          <p>
+            - GitHub: <a href="https://github.com/r-k1mur4">https://github.com/r-k1mur4</a>
+          </p>
+          <p>
+            ▼ 保有資格<br></br>
+            &emsp;- WEBクリエイター能力認定試験 エキスパート<br></br>
+            &emsp;- TOEIC L&R 865点
+          </p>
         </div>
         <div className="about-img">
           <Image
